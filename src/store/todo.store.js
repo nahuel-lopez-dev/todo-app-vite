c46989af81e1f41c601191a666e97ed5e7838f5f
Nahuel <nahuel.profesor@gmail.com>
2023-04-21 18:48:58 -0300

    store inicializado

---
+++ src/store/todo.store.js
@@ -0,0 +1,26 @@
+import { Todo } from "../todos/models/todo.model"
+
+const Filters = {
+    All: 'all',
+    Completed: 'Completed',
+    Pending: 'Pending'
+}
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+    ],
+    filter: Filters.All,
+}
+
+const initStore = () => {
+    console.log(state);
+    console.log('InitStore ⚡');
+}
+
+export default {
+    initStore,
+}
+
