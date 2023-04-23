f351372227efa67d63336cc1cb7601046564222b
Nahuel <nahuel.profesor@gmail.com>
2023-04-22 22:01:13 -0300

    métodos que se van a implementar en el store, agregados

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,7 +20,50 @@ const initStore = () => {
     console.log('InitStore ⚡');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId todo identifier
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
 
